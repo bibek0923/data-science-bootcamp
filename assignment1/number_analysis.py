e35331c4ba6261485b69db7b9ca2db91c4ed84b2
bibek0923 <panerubibek0923@gmail.com>
2024-06-17 20:53:11 +0545

    number analysi

--- assignment1/number_analysis.py
+++ assignment1/number_analysis.py
@@ -8,4 +8,13 @@ else :
 
 for i in range(1,entered_number+1,1):
     if i%2==0:
+        print(i)
+
+sum=0
+while(entered_number!=0):
+   
+    sum = sum + entered_number
+    entered_number=entered_number-1
+    
+print(f"sum is {sum}")
         
