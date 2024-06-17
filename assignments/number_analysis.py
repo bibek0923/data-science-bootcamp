40f3cb743c50713a6b92aca0af55926f4b971a3b
bibek0923 <panerubibek0923@gmail.com>
2024-06-17 20:29:22 +0545

    complete variable analysis

---
+++ assignments/number_analysis.py
@@ -0,0 +1,11 @@
+entered_number = int(input("enter a number :"))
+print(type(entered_number))
+if entered_number % 2==0:
+    print("even")
+else :
+    print("odd")
+    
+
+for i in range(1,entered_number+1,1):
+    if i%2==0:
+        
---
+++ assignments/variable_manipulation.py
@@ -0,0 +1,15 @@
+birth_year = 2003
+height= 173.736
+name="bibek"
+employment_status=False
+print(f"birthyear is {birth_year}")
+print(type(birth_year))
+
+print(f"heighy is {height}")
+print(type(height))
+
+print(f"name is {name}")
+print(type(name))
+
+print(f"employment status is {employment_status}")
+print(type(employment_status))
